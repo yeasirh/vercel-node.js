342bf42dff804380eb389e37eff9c9f428092822
yeasirh <yeasirh@gmail.com>
2023-04-17 12:42:10 +1000

    updated code for testing

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
-module.exports = (res) => {
-	res.status(200).send(`Hello World!`);
+// module.exports = (res) => {
+// 	res.status(200).send(`Hello World!`);
+// };
+
+module.exports = (req, res) => {
+	const name = req.query.name || "stranger";
+	res.status(200).send(`Hello, ${name}!`);
 };
