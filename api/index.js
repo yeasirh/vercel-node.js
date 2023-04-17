f450c91b69c2c20699489f944194a5ea26bcefa6
yeasirh <yeasirh@gmail.com>
2023-04-17 12:50:56 +1000

    code updated

--- api/index.js
+++ api/index.js
@@ -1,15 +1,6 @@
-// module.exports = (res) => {
-// 	res.status(200).send(`Hello World!`);
-// };
-
-// module.exports = (req, res) => {
-// 	const name = req.query.name || "stranger";
-// 	res.status(200).send(`Hello, ${name}!`);
-// };
-
 export default function handler(request, response) {
 	// api/[name].js -> /api/lee
 	// request.query.name -> "lee"
-	// const { name } = request.query;
-	return response.end(`Hello World!`);
+	const { name } = request.query;
+	return response.end(`Hello ${name}!`);
 }
