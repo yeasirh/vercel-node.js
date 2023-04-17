0547dc5db5597af8ea3bf1194fd7f981a9152c68
yeasirh <yeasirh@gmail.com>
2023-04-17 12:48:05 +1000

    default exported function added

--- api/index.js
+++ api/index.js
@@ -2,7 +2,14 @@
 // 	res.status(200).send(`Hello World!`);
 // };
 
-module.exports = (req, res) => {
-	const name = req.query.name || "stranger";
-	res.status(200).send(`Hello, ${name}!`);
-};
+// module.exports = (req, res) => {
+// 	const name = req.query.name || "stranger";
+// 	res.status(200).send(`Hello, ${name}!`);
+// };
+
+export default function handler(request, response) {
+	// api/[name].js -> /api/lee
+	// request.query.name -> "lee"
+	// const { name } = request.query;
+	return response.end(`Hello World!`);
+}
